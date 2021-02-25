2a21b4cb406286ea836458eac31818c87339a457
Tyler Kuromiya Parker <tylerkkp@gmail.com>
2021-02-25 12:14:25 -0800

    added watchlist.py updated av_test.py to use watchlist values

--- scripts/av_test.py
+++ scripts/av_test.py
@@ -1,20 +1,34 @@
 # Import API key data for user from config.py file
 import config
+# Import watchlist from watchlist.py
+import watchlist
 
 import requests
 
-# Example API request to Alpha Vantage:
-# https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo
+"""
+This script uses Alpha Vantage, which limits API calls to:
+5 API requests per minute; 500 API requests per day
+
+Example API request to Alpha Vantage:
+https://www.alphavantage.co/query?function=OVERVIEW&symbol=IBM&apikey=demo
+
+See more options for API calls on the Alpha Vantage documentation page:
+https://www.alphavantage.co/documentation/#
+"""
+
 function = 'OVERVIEW'
-symbol = 'IBM'
+#symbol = 'FLIR'
 apikey = config.api_key
 
-response = requests.get("https://www.alphavantage.co/query" + 
-                        "?function=" + function + 
-                        "&symbol=" + symbol + 
-                        "&apikey=" + apikey)
+for i in watchlist.ticks:
+    symbol = i
+    response = requests.get("https://www.alphavantage.co/query" + 
+                            "?function=" + function + 
+                            "&symbol=" + symbol + 
+                            "&apikey=" + apikey)
 
-data = response.json()
+    data = response.json()
 
-print('Symbol: ', data['Symbol'])
-print('PE Ratio: ', data['PERatio'])
+    print('Symbol: ', data['Symbol'])
+    print('PE Ratio: ', data['PERatio'], '\n')
+
---
+++ scripts/watchlist.py
@@ -0,0 +1,3 @@
+# Store watchlist as strings in list 'ticks'
+
+ticks = ['AAPL', 'V', 'SBUX', 'TWTR', 'WSM']
